93d557a38d0fa047ca0f3eb87334eba3e8d8c022
nicolastradiotto2 <nicolastradiotto2@gmail.com>
2025-09-01 19:18:02 +0200

    Giorno 4: Prime variabili e operatori in JS

---
+++ week1/js/script.js
@@ -0,0 +1,11 @@
+// Variabili
+let nome = "Nicola";   // variabile che può cambiare
+const eta = 23;        // variabile costante
+var citta = "Treviso"; // vecchia sintassi, evita di usarla
+let nomeUtente = prompt("Come ti chiami?");
+
+console.log(nome, eta, citta);
+
+
+
+document.write("<h2>Ciao " + nomeUtente + "!</h2>");
