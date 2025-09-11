b4b5400cf1bfc0c2daaa366f142772851a86aca5
nicolastradiotto2 <nicolastradiotto2@gmail.com>
2025-09-11 22:26:28 +0200

    Init React app con Vite: struttura base + saluto JSX

---
+++ week3/react/react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  const nome = "Nicola";
+
+  return (
+    <div>
+      <h1>Ciao {nome}, Benvenuto su React!</h1>
+      <p>Prima prova app vite</p>
+    </div>
+  )
+}
+
+export default App
