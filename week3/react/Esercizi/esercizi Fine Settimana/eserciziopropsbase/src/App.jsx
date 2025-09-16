abb4d4ce8109a462067262e1c36c61ffdc11245c
nicolastradiotto2 <nicolastradiotto2@gmail.com>
2025-09-16 22:02:08 +0200

    Esercizio 1: componente Messaggio con props

---
+++ week3/react/Esercizi/esercizi Fine Settimana/eserciziopropsbase/src/App.jsx
@@ -0,0 +1,13 @@
+import Messaggio from "./components/Messaggio"
+
+function App() {
+return (
+    <>
+      <div>
+          <Messaggio nome = "Nicola" messaggio="Sono bravo"></Messaggio>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ week3/react/Esercizi/esercizi Fine Settimana/eserciziopropsbase/src/components/Messaggio.jsx
@@ -0,0 +1,8 @@
+function Messaggio(props){
+    return (
+        <p>Ciao {props.nome}!, Il tuo messaggio è: {props.messaggio}</p>
+
+
+    )
+}
+export default Messaggio;
