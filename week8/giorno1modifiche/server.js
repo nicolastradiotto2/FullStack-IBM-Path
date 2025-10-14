dfd1c5520b6328711e46b05cb522114bd16fc9ad
nicolastradiotto2 <nicolastradiotto2@gmail.com>
2025-10-14 23:05:57 +0200

    fix cors + aggiunto vercel.json

--- week8/giorno1modifiche/server.js
+++ week8/giorno1modifiche/server.js
@@ -8,36 +8,44 @@ const { notFound, errorHandler } = require("./middleware/error");
 
 const app = express();
 
-
+//  CORS 
 app.use(cors({
   origin: [
     'http://localhost:4000',   
-    'http://localhost:5500'    
+    'http://localhost:5500',   
+    'https://full-stack-ibm-path-6ezx.vercel.app' 
   ],
-  credentials: false
+  credentials: true
 }));
 
+//  Middleware JSON
 app.use(express.json());
 
-
+//  Healthcheck 
 app.get("/api/health", (req, res) => res.json({ ok: true }));
 
-
+//  Rotte API
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/products", productRoutes);
 
-
+//  Static )
 app.use(express.static("public"));
 
+//  Rotta base
 app.get("/", (req, res) => {
   res.send("server funzionante + mongo funzionante");
 });
 
+//  Gestione errori
 app.use(notFound);
 app.use(errorHandler);
 
+//  Porta dinamica 
 const PORT = process.env.PORT || 4000;
 
+//  Connessione al DB + avvio server
 connectDB(process.env.MONGO_URI).then(() => {
-  app.listen(PORT, () => console.log(` Server attivo su http://localhost:${PORT}`));
+  app.listen(PORT, () =>
+    console.log(`🌍 Server attivo su http://localhost:${PORT}`)
+  );
 });
